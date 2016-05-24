9290c442865d4faa0cfd186e4cb460b804c0e1bf
Sophie_Wang <anxin_wang@126.com>
2016-05-24 19:36:56 +0800

    ddd

--- jquery/plugin_frame.js
+++ jquery/plugin_frame.js
@@ -1,22 +1,75 @@
 /**
  * Created by Administrator on 2015/9/16.
  */
-(function () {
++function ($) {
     'use strict';
-    var $;
     /*==================================================
      对象类
      ====================================================*/
-    var Plugin_Name = function (container, params) {
+    var Plugin_Name = function (element, options) {
+        this.$element      = $(element);
+        this.options       = $.extend({}, Plugin_Name.DEFAULTS, options);
+        //...定义对象的其他变量
 
     };
+
+    //定义类的静态变量
+    Plugin_Name.VERSION  = '3.3.2'
+    Plugin_Name.DEFAULTS = {
+        toggle: true,
+        trigger: '[data-toggle="collapse"]'
+    }
+
+
+
     /*==================================================
      Prototype
      ====================================================*/
     Plugin_Name.prototype={}
 
-    window.Plugin_Name = Plugin_Name;
-})();
+
+
+    // BUTTON PLUGIN DEFINITION
+    // ========================
+
+    function Plugin(option) {
+        return this.each(function () {
+            var $this   = $(this)
+            //html上的data标签扩展，可作为参数
+            var data    = $this.data('bs.Plugin_Name')
+            var options = typeof option == 'object' && option
+
+            if (!data) $this.data('bs.Plugin_Name', (data = new Plugin_Name(this, options)))
+
+            if (typeof option == 'string') data[option]()
+
+
+            //if (option == 'toggle') data.toggle()
+            //else if (option) data.setState(option)
+        })
+    }
+
+    var old = $.fn.Plugin_Name
+
+    $.fn.Plugin_Name             = Plugin
+    $.fn.Plugin_Name.Constructor = Plugin_Name
+
+
+    // BUTTON NO CONFLICT
+    // ==================
+
+    $.fn.button.noConflict = function () {
+        $.fn.button = old
+        return this
+    }
+
+
+
+}(jQuery);
+
+
+
+
 
 /*===========================
   AMD Export
